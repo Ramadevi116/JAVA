784f614c5fb03574395595ab6ba270ef38deb3bd
Ramadevi116 <89075054+Ramadevi116@users.noreply.github.com>
2023-07-04 11:00:18 +0530

    java

---
+++ Java/p. Control Statements/Code.java
@@ -0,0 +1,31 @@
+User input:
+import java.util.*;
+class main{
+    public static void main(String arg[]){
+        for(int j=1;j<=1;j++){
+            System.out.print("Enter number ");
+        Scanner i=new Scanner(System.in);
+        int x=i.nextInt();
+        System.out.println(x);
+        
+        }
+    }
+}
+
+for loop:
+-->patterns
+import java.util.*;
+class main{
+    public static void main(String arg[]){
+        Scanner i=new Scanner(System.in);
+        System.out.print("Enter the count:");
+        int x=i.nextInt();
+        for(int j=1;j<=x;j++){
+            for(int k=1;k<=x;k++){
+            System.out.print("Rama ");
+        }
+    
+    System.out.println();
+}
+}
+}
