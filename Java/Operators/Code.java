dd9a277b60da8509da0395e83b2aa6c577624304
Ramadevi116 <89075054+Ramadevi116@users.noreply.github.com>
2023-07-04 11:08:31 +0530

    java

---
+++ Java/Operators/Code.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main(String arg[]){
+        
+    //pre increment
+    int x=10;
+    System.out.println(++x); // 11
+    //post increment
+    int y=10;
+    System.out.println(y++);// 10
+    System.out.println(y); // 11
+    }
+}
